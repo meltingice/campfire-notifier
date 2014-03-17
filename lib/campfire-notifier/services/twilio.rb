052f7222f8d582319b12752b0312b252bb9f25ea
Ryan LeFevre <meltingice8917@gmail.com>
2014-03-17 17:36:05 -0400

    Fix twilio

--- lib/campfire-notifier/services/twilio.rb
+++ lib/campfire-notifier/services/twilio.rb
@@ -4,17 +4,21 @@ module CampfireNotifier
       has_config :twilio
     
       def notify!
-        CampfireNotifier.logger.info "Notifying #{person.name} at #{person.phone}:"
+        CampfireNotifier.logger.info "Notifying #{person.name} at #{phone_number}:"
         CampfireNotifier.logger.info formatted_message
 
         client.account.messages.create({
           from: config['phone_number'],
-          to: person.config['phone'],
+          to: phone_number,
           body: formatted_message.truncate(140)
         })
       end
 
       private
+
+      def phone_number
+        person.config['phone']
+      end
 
       def client
         @client ||= ::Twilio::REST::Client.new(
